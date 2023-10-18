0d4a48bc7a9812c186602ccffd6c47c16571116e
Declan Ware <declanware@gmail.com>
2023-10-18 15:55:00 -0400

    added prototype messages bar

--- src/components/Messaging.jsx
+++ src/components/Messaging.jsx
@@ -1,11 +1,23 @@
 import './Messaging.css'
+import React, { useState, useEffect } from 'react';
 
 const Messaging = (props)=>{
 
-    console.log("messaging initialised")
+    const [messages, setMessages] = useState('');
+    useEffect(() => {
+        fetch(props.messages) // Assuming props.messages contains the path to your text file
+        .then((response) => response.text())
+        .then((text) => {
+            setMessages(text);
+        })
+        .catch((error) => {
+            console.error('Error fetching the file:', error);
+        });
+    }, [props.messages]);
+
     return (
         <div class = "Messaging">
-            <p>{props.messages}</p>
+            <p>{messages}</p>
         </div>
     )
 }
--- src/pages/Store.jsx
+++ src/pages/Store.jsx
@@ -1,26 +1,25 @@
 import NavBar from "../components/NavBar.jsx"
 import Listing from "../components/Listing.jsx"
 import Messaging from "../components/Messaging.jsx"
+import Listings from "../components/Listings.jsx"
 import './Store.css'
 
 //assets
-import HondaCivic from '../assets/HondaCivic.jpg';
+import SampleMessages from '../assets/SampleMessages.txt';
 
 
 function Store() {
     return (
     
-    <div>
-        <NavBar />
-        <p class="spacer"></p>
-        <Messaging />
-        <div class="spacer"></div>
-        <div class="listings">
-            {Array(25).fill(null).map(() => (
-                <Listing image={HondaCivic} title="Honda Civic" description="2012 honda civic 150000 miles good condition leather interior heated seats lorem ipsum dolor el sedat in nominum lorem ipsum dolor el sedatg in nominum lorem ipsum"></Listing>
-            ))}
-        </div>
+    <div class = "store">
         
+
+        <div class = "content">
+            <Messaging messages={SampleMessages}/>
+            <Listings />
+        </div>
+
+        <NavBar />
     </div>)
 }
 
