ea2eb9104a71d18e540e6a9d9fe9359c18b16239
brp00005 <brp00005@mix.wvu.edu>
2023-12-14 08:47:36 -0500

    final comments

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -1,45 +1,78 @@
+// Author: Hunter Harris
+
+// Import necessary modules and components
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { Card, CardContent, CardHeader, Avatar, Typography, Button, Grid, Box } from '@mui/material';
+import { 
+  Card, 
+  CardContent, 
+  CardHeader, 
+  Avatar, 
+  Typography, 
+  Button, 
+  Grid, 
+  Box 
+} from '@mui/material';
 import EditIcon from '@mui/icons-material/Edit';
 import EmailIcon from '@mui/icons-material/Email';
 import { useAuth } from '../../components/newAuth/AuthContext';
 import Listing2 from '../../components/Listing2/listing2';
-import { doc, setDoc, getDoc, getDocs, getFirestore, onSnapshot, addDoc, orderBy, query, where, serverTimestamp, Timestamp, collection } from 'firebase/firestore';
+import { 
+  doc, 
+  setDoc, 
+  getDoc, 
+  getDocs, 
+  getFirestore, 
+  onSnapshot, 
+  addDoc, 
+  orderBy, 
+  query, 
+  where, 
+  serverTimestamp, 
+  Timestamp, 
+  collection 
+} from 'firebase/firestore';
 import { auth, firestore } from '../../firebase.js';
 
+// Define the Profile component
 const Profile = () => {
+  // States to manage user data and listings
   const [username, setUsername] = useState('');
   const [profileImage, setProfileImage] = useState('');
   const [userListings, setUserListings] = useState([]); // State to store user's listings
-  const navigate = useNavigate();
+  const navigate = useNavigate(); // Hook to manage navigation
 
+  // Function to handle edit profile navigation
   const handleEditProfile = () => {
     navigate('/EditProfile');
   };
 
+  // Function to handle messaging navigation
   const handleMessaging = () => {
     navigate('/Messaging');
   };
 
+  // Accessing user information through the useAuth hook
   const { user } = useAuth();
 
+  // Fetch user data and listings on component mount or user change
   useEffect(() => {
     const fetchUserData = async () => {
       if (user) {
-        // User is signed in, update profile information
+        // Update profile information if user is signed in
         setUsername(user.displayName || 'Default Username');
         setProfileImage(user.photoURL || '');
 
-        // Fetch user's listings (replace 'userIdField' with the actual field in your listing documents)
+        // Fetch user's listings based on user ID
         const userListingQuery = await getDocs(
           query(collection(firestore, 'listings'), where('userID', '==', user.uid))
         );
 
+        // Extract listing data from fetched documents and update state
         const listings = userListingQuery.docs.map((doc) => doc.data());
         setUserListings(listings);
       } else {
-        // User is signed out, reset profile information
+        // Reset profile information if user is signed out
         setUsername('');
         setProfileImage('');
         setUserListings([]);
@@ -47,7 +80,7 @@ const Profile = () => {
     };
 
     fetchUserData();
-  }, [user]);
+  }, [user]); // Dependency array to execute the effect when the 'user' object changes
 
   return (
     <Box mt={5}>
@@ -55,6 +88,7 @@ const Profile = () => {
         <Grid item xs={12} sm={8} md={6} lg={4}>
           <Card>
             <CardHeader
+              // Display user's profile avatar and username
               avatar={<Avatar src={profileImage || 'https://via.placeholder.com/155'} alt="Profile" />}
               title={
                 <Typography variant="h4" color="textPrimary">
@@ -66,6 +100,7 @@ const Profile = () => {
             <Box p={2}>
               <Grid container spacing={2}>
                 <Grid item xs={6}>
+                  {/* Button to initiate messaging */}
                   <Button
                     fullWidth
                     variant="outlined"
@@ -76,6 +111,7 @@ const Profile = () => {
                   </Button>
                 </Grid>
                 <Grid item xs={6}>
+                  {/* Button to navigate to edit profile */}
                   <Button
                     fullWidth
                     variant="contained"
@@ -93,13 +129,21 @@ const Profile = () => {
       </Grid>
       <Box mt={2}>
         <Typography variant="h6" color="textPrimary">
+          {/* Display user's listings */}
           {username}'s listings
         </Typography>
         <hr />
         <Grid container spacing={1}>
+            {/* Display user's listings using Listing2 component */}
             {userListings.map((listing) => (
-                <Grid item xs={12} sm={4} md={3} lg={2}>
-                    <Listing2 img={listing.image} title={listing.title} content={listing.content} userID={listing.userID} postID={listing.postID}></Listing2>
+                <Grid item xs={12} sm={4} md={3} lg={2} key={listing.postID}>
+                    <Listing2 
+                      img={listing.image} 
+                      title={listing.title} 
+                      content={listing.content} 
+                      userID={listing.userID} 
+                      postID={listing.postID}
+                    />
                 </Grid>
             ))}
         </Grid>
